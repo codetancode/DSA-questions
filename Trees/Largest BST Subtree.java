1934fe584f3943f9010465a16caacdd03c61e9fb
codetancode <tanay20gupta18@gmail.com>
2022-01-30 13:42:28 +0530

    Largest BST Subtree O(n, n)

--- Trees/Largest BST Subtree.java
+++ Trees/Largest BST Subtree.java
@@ -1,72 +1,44 @@
-Q3. Largest BST Subtree
-S
-Problem Description
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
+public class Solution {
+    static int maxSize = 1;
+    // max possible and min possible range for root value to be BST, 
+    // boolean isBst for checking all subtree
+    // size varable for counting node
+    static int[] isLargestBST(TreeNode root){   
+        //in the array returning [ max, min, isBST, size] all int
+        if(root==null){return new int[]{Integer.MIN_VALUE, Integer.MAX_VALUE, 1, 0}; }
+        int[] lrange = isLargestBST(root.left);
+        int[] rrange = isLargestBST(root.right);
+            
+        // if root val > left ka max and root val is < right ka min, also check if left and right both are bst
+        if(root.val > lrange[0] && root.val < rrange[1] && lrange[2]==1 && rrange[2]==1){
+            //current node + size of left and right
+            int tempSize = 1 + lrange[3]+rrange[3];
+            maxSize = Math.max(maxSize, tempSize);
+            int[] range = {Math.max(root.val, rrange[0]), Math.min(root.val, lrange[1]), 1, tempSize};
+            return range;
+        
+        }
 
-Given a Binary Tree A with N nodes.
-
-Write a function that returns the size of the largest subtree which is also a Binary Search Tree (BST).
-
-If the complete Binary Tree is BST, then return the size of whole tree.
-
-NOTE:
-
-Largest subtree means subtree with most number of nodes.
-
-
-Problem Constraints
-
-1 <= N <= 105
-
-
-
-Input Format
-
-First and only argument is an pointer to root of the binary tree A.
-
-
-
-Output Format
-
-Return an single integer denoting the size of the largest subtree which is also a BST.
-
-
-
-Example Input
-
-Input 1:
-
-     10
-    / \
-   5  15
-  / \   \
- 1   8   7
-Input 2:
-
-     5
-    / \
-   3   8
-  / \ / \
- 1  4 7  9
-
-
-Example Output
-
-Output 1:
-
- 3
-Output 2:
-
- 7
-
-
-Example Explanation
-
-Explanation 1:
-
- Largest BST subtree is
-                            5
-                           / \
-                          1   8
-Explanation 2:
-
- Given binary tree itself is BST.
+        int[] range = {Math.max(root.val, rrange[0]), Math.min(root.val, lrange[1]), 0, 0};
+        return range;
+    }
+    public int solve(TreeNode A) {
+        //atleast will have have 1 node as BST
+        maxSize=1;
+        isLargestBST(A);
+        return maxSize;
+    }
+}
