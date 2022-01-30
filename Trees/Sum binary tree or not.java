5daaf82d7164a02a811dc9625bec7b348f281142
codetancode <tanay20gupta18@gmail.com>
2022-01-30 12:36:13 +0530

    'O(n,n)'

--- Trees/Sum binary tree or not.java
+++ Trees/Sum binary tree or not.java
@@ -12,34 +12,36 @@
  * }
  */
 public class Solution {
-    //this is  O(n2, n) approach, need O(n, n) using pair and not recalculating sum again and again
-    static int sumCheck(TreeNode root){
-        // if leaf node or null valid so return 1
-        if(root == null){return 1;}
-        if(root.left==null && root.right == null){return 1;}
-
-        // get left and right sum
-        int left = sum(root.left);
-        int right = sum(root.right);
-
-        // if left and right sum == current root
-        int temp = (left+right == root.val)?1:0;
-
-        // if left and right sum == current root && left subtree and right subtree are also having sum == root val, then return 1
-        if( ( temp & sumCheck(root.left)  & sumCheck(root.right) ) == 1 ){
-            return 1;
+    static class Pair{
+        int sumTillNow;
+        boolean isSumT;
+        Pair(int sum, boolean isSumT){
+            this.sumTillNow = sum;
+            this.isSumT=isSumT;
         }
-        //if not previously true return 0
-        return 0;
     }
 
-    // for getting left subtree sum and right subtree sum
-    static int sum(TreeNode root){
-        if(root==null){return 0;}
-        return root.val+sum(root.left)+sum(root.right);
+
+    //Using pair and not recalculating sum again and again O(n, n)
+    static Pair sumCheck(TreeNode root){
+        // if leaf node or null valid so return 1
+        if(root == null){return new Pair(0, true);}
+        if(root.left==null && root.right == null){return new Pair(root.val, true);}
+
+        Pair left = sumCheck(root.left);
+        Pair right = sumCheck(root.right);
+
+        //if sum == to current root and leftsubtree and right subtree are true(isSum binary)
+        if(root.val == left.sumTillNow+right.sumTillNow && left.isSumT && right.isSumT){
+            return new Pair(root.val+left.sumTillNow+right.sumTillNow, true);
+        }
+
+        // if not returned true earlier return false
+        return new Pair(root.val+left.sumTillNow+right.sumTillNow, false);
     }
 
     public int solve(TreeNode A) {
-        return sumCheck(A);
+        Pair lastReturn = sumCheck(A);
+        return lastReturn.isSumT?1:0;
     }
 }
