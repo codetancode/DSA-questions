f671965a9c5bcb4e2f26d47723dc569c335f578a
codetancode <tanay20gupta18@gmail.com>
2022-01-30 16:43:09 +0530

    Kth Smallest Element In BST O(n, 1)

--- Trees/Kth Smallest Element In BST.java
+++ Trees/Kth Smallest Element In BST.java
@@ -1,30 +1,45 @@
 /**
  * Definition for binary tree
  * class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) {
- *      val = x;
- *      left=null;
- *      right=null;
- *     }
- * }
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) {
+             * val = x;
+             * left = null;
+             * right = null;
+             *
+         }
+         *
+ }
  */
-public class Solution {
-    //bruteforce, store inorder travesal and return k-1 index
-    static void inorderStore(TreeNode root, ArrayList<Integer> inO){
-        if(root == null){return;}
-        inorderStore(root.left, inO);
-        inO.add(root.val);
-        inorderStore(root.right, inO);
+ //going inorder and keeping count till k
+ public class Solution {
+     static int k = 0;
 
-    }
-    public int kthsmallest(TreeNode A, int B) {
-        //InOrder of BST is always sorted
-        // brute force
-        ArrayList<Integer> inO = new ArrayList<>();
-        inorderStore( A, inO);
-        return inO.get(B-1);
-    }
-}
+     public int kthsmallest(TreeNode A, int B) {
+         //reinitialize gobal k for multiple test cases
+         k = B;
+         return find(A);
+     }
+     public static int find(TreeNode root) {
+         if (root == null)
+             return -1;
+         // We do an inorder traversal here. so root.left func call
+         int k1 = find(root.left);
+         //if in left subtree we found k=0
+         if (k == 0){
+         // left subtree has k or more elements.
+             return k1;
+         }
+
+         //reduce k for current node
+         k--;
+         //if k==0 we found our root val
+         if (k == 0){
+             return root.val; // root is the kth element.
+         }
+         //else going root.right as per inorders traversal
+         return find(root.right); // answer lies in the right node.
+     }
+ }
