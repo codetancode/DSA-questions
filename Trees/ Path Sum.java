e7f044bc32fd711cb7f25f830e3cefbeee1ea827
codetancode <tanay20gupta18@gmail.com>
2022-01-30 12:40:38 +0530

    '/**'

---
+++ Trees/ Path Sum.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
+public class Solution {
+    int pathSum0Ornot(TreeNode root, int sum){
+        if(root==null){return 0;}
+        //reduce sum for this function call and check if root is the leaft node then return
+        sum-=root.val;
+        if(root.left==null && root.right==null){
+            return sum==0?1:0;
+        }
+        return pathSum0Ornot(root.left, sum) | pathSum0Ornot(root.right, sum);
+    }
+    public int hasPathSum(TreeNode A, int B) {
+        return pathSum0Ornot(A, B);
+    }
+}
