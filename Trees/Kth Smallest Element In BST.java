9818e31b14330bc91b1745ce8a484f62a3a30d95
codetancode <tanay20gupta18@gmail.com>
2022-01-30 16:24:40 +0530

    Kth Smallest Element In BST O(n, n)

--- Trees/Kth Smallest Element In BST.java
+++ Trees/Kth Smallest Element In BST.java
@@ -1,59 +1,30 @@
-Q4. Kth Smallest Element In BST
-Problem Description
-Given a binary search tree represented by root A, write a function to find the Bth smallest element in the tree.
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
+public class Solution {
+    //bruteforce, store inorder travesal and return k-1 index
+    static void inorderStore(TreeNode root, ArrayList<Integer> inO){
+        if(root == null){return;}
+        inorderStore(root.left, inO);
+        inO.add(root.val);
+        inorderStore(root.right, inO);
 
-
-
-Problem Constraints
-1 <= Number of nodes in binary tree <= 100000
-
-0 <= node values <= 10^9
-
-
-
-Input Format
-First and only argument is head of the binary tree A.
-
-
-
-Output Format
-Return an integer, representing the Bth element.
-
-
-
-Example Input
-Input 1:
-
-
-            2
-          /   \
-         1    3
-B = 2
-Input 2:
-
-
-            3
-           /
-          2
-         /
-        1
-B = 1
-
-
-
-Example Output
-Output 1:
-
- 2
-Output 2:
-
- 1
-
-
-Example Explanation
-Explanation 1:
-
-2nd element is 2.
-Explanation 2:
-
-1st element is 1.
+    }
+    public int kthsmallest(TreeNode A, int B) {
+        //InOrder of BST is always sorted
+        // brute force
+        ArrayList<Integer> inO = new ArrayList<>();
+        inorderStore( A, inO);
+        return inO.get(B-1);
+    }
+}
