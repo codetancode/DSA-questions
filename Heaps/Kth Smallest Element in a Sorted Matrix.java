f8e19a0fbe67ea07fd8e408c8ad251cac7392ac3
codetancode <tanay20gupta18@gmail.com>
2022-02-01 15:57:08 +0530

    Kth Smallest Element in a Sorted Matrix O(max(m, n)*logn, m+n)BF

---
+++ Heaps/Kth Smallest Element in a Sorted Matrix.java
@@ -0,0 +1,20 @@
+public class Solution {
+    //O(max(m, n)*logn, m+n)
+    //basic idea fill all in minheap, take out bth element
+    public int solve(ArrayList<ArrayList<Integer>> A, int B) {
+        PriorityQueue<Integer> minheap = new PriorityQueue<>();
+        //fill all element in heap
+        for(int i=0;i < A.size();i++){
+            for(int j=0;j < A.get(0).size();j++){
+                minheap.add(A.get(i).get(j));
+            }
+        }
+
+        // take out B-1 elements
+        for(int i=0;i < B-1;i++){
+            minheap.poll();
+        }
+        // bth elements
+        return minheap.peek();
+    }
+}
