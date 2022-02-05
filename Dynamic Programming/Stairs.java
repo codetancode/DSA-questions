0d0c9d37335e8bb595f21ca6723625f8bfc88669
codetancode <tanay20gupta18@gmail.com>
2022-02-05 15:19:25 +0530

    Stairs O(n, 1)

--- Dynamic Programming/Stairs.java
+++ Dynamic Programming/Stairs.java
@@ -1,18 +1,20 @@
 public class Solution {
-    //recursive dp structure
-    //O(2^n, n)
-    //1 function calling 2 function similar to fibonacii
-    //recur. stach space of n
-    int dprecursive(int stair){
-        //point to note here is when stairs == 0, then also return 1 cox WAYS
-        //in 1 way stairs ==0
-        if(stair == 0){return 1;}
-        if(stair == 1){return 1;}
-        //possible ways for stair-1 + possible ways for stairs-2
-        return dprecursive(stair-1)+dprecursive(stair-2);
-
-    }
+    //irrtative O(n, 1)
     public int climbStairs(int A) {
-        return dprecursive(A);
+        //point to note here is at stair 0 ways is 1
+        //point to note here is at stair 1 ways is 1
+        // now using Dp(knowledge of ways for 0 staris and 1 stair calculate ways for A)
+        int start0 = 1;
+        int start1 = 1;
+        int ans=1;
+        //now going for stairs 2 till A, temp in the end will have answer for A
+        for(int i=2;i <= A;i++){
+            ans = start0 + start1;
+            //moving forward updating values
+            start0 = start1;
+            start1 = ans;
+        }
+
+        return ans;
     }
 }
