4245aafa4b09a59d031586777426dd7125fb9681
codetancode <tanay20gupta18@gmail.com>
2022-02-07 11:37:22 +0530

    Longest Balanced Substring O(n, n)

--- Dynamic Programming/Longest Balanced Substring.java
+++ Dynamic Programming/Longest Balanced Substring.java
@@ -1,44 +1,40 @@
 public class Solution {
-    // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
-
-    //idea is to go through all possible index pairO(n^2), check if valid O(n), then update max answer
-    boolean validateString(String A, int from, int to){
-        Stack<Character> st = new Stack<>();
-        for(int i=from;i <= to;i++){
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
+    public int LBSlength(final String A) {
+        //O(n, n) solution using stack
+        //idea is to store indexes of string and update max for valid string
+        if(A.length() <= 1){
+            return 0;
+        }
+        Stack<Integer> st = new Stack<>();
+        st.push(-1);
+        //pushing -1 for empty stack, or imbalanced situation(more closing bracket)
+        int ans = 0;
+        for(int i=0;i < A.length();i++){
             char temp = A.charAt(i);
 
-            if(!st.isEmpty()){
-                //if st has opening and matching with new closing, pop opening
-                if(temp == ')' && st.peek() == '(' || temp == ']' && st.peek() == '[' || temp == '}' && st.peek() == '{'){
-                    st.pop();//pop corresponding closing
-                }else{
-                    //if not closing or not matching add them anyway
-                    st.push(temp);
-                }
+            // if opening push index into stack
+            if(temp == '(' || temp == '{' || temp =='['){
+                st.push(i);
             }else{
-                //if 1st is closing return false(invalid)
-                if(temp == ')' || temp == '}' || temp == ']'){
-                    return false;
-                }
-                //add opening
-                st.push(temp);
-            }
-        }
-        //return if stack size ==0  boolean
-        return st.size()==0;
-    }
-
-    public int LBSlength(final String A) {
-        int res = 0;
-        for(int i=0;i < A.length();i++){
-            for(int j=i;j < A.length();j++){
-                if(validateString(A, i, j)){
-                    //update answer
-                    res = Math.max(res, j-i+1);
+                //else if closing,
+                //if stack stack peek is not -1, and
+                // either of peek index is corrosponding opening for new closing char, pop opening index, get max
+                if( (st.peek() != -1) &&(
+                    (temp == '}' && A.charAt(st.peek()) == '{' ) ||
+                    (temp == ']' && A.charAt(st.peek()) == '[' ) ||
+                    (temp == ')' && A.charAt(st.peek()) == '(' )
+                    ) ){
+                    st.pop();
+                    //as found valid index sequence wrt i and peek
+                                // curr i - 1 index less than opening will give the difference(length of valid string)
+                    ans = Math.max(ans, i-st.peek());
+                }else{
+                    //if closing bracket(not matching with peek)invalid string
+                    st.push(i);
                 }
             }
         }
-
-        return res;
+        return ans;
     }
 }
