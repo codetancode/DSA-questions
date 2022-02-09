d010d60a9ce9e787f55b97a252b23a0cb3a1750c
codetancode <tanay20gupta18@gmail.com>
2022-02-09 17:35:57 +0530

    Spelling Checker O(n, 1)using trie

--- Tries/Spelling Checker.java
+++ Tries/Spelling Checker.java
@@ -1,18 +1,70 @@
 public class Solution {
-    //O(n, n)using simple set of strings
-    public ArrayList<Integer> solve(ArrayList<String> A, ArrayList<String> B) {
-        ArrayList<Integer> res = new ArrayList<Integer>();
-        HashSet<String> setOfString = new HashSet<>();
-        //fill dic words then check with B
-        for(int i=0;i < A.size();i++){
-            setOfString.add(A.get(i));
+    // O(n, 1) space is optimized and clost to 1 as constant char size
+    //idea is to implement tries for words,
+    // every trie node will have child Trienodes of 26 size, and a boolean val for marking end of word
+    static TrieNode root;
+    static class TrieNode {
+        TrieNode[] children;
+        boolean isEndOfWord;
+
+        TrieNode() {
+            this.isEndOfWord = false;
+            this.children = new TrieNode[26];
         }
-        for(int i=0;i < B.size();i++){
-            //if present then add 1 else add 0
-            if(setOfString.contains(B.get(i))){
-                res.add(1);
+    }
+
+    static void insert(String key) {
+        int level;
+        int length = key.length();
+        int index;
+        TrieNode rootTrav = root;
+        //taking static global root as root, checking for char at every level
+        for (level = 0; level < length; level++) {
+            index = key.charAt(level) - 'a';
+            //if node for that char not present add new node
+            if(rootTrav.children[index] == null){
+                rootTrav.children[index] = new TrieNode();
+            }
+            //go ot new node
+            rootTrav = rootTrav.children[index];
+        }
+        //mark end of word as true
+        rootTrav.isEndOfWord = true;
+    }
+
+    static boolean search(String key) {
+        int level;
+        int length = key.length();
+        int index;
+        TrieNode rootTrav = root;
+        //now checking if every char is present or not, if in middle found null return false
+        for (level = 0; level < length; level++) {
+            index = key.charAt(level) - 'a';
+            if (rootTrav.children[index] == null){
+                //if null found, false
+                return false;
+            }
+            //else go to the end
+            rootTrav = rootTrav.children[index];
+        }
+        // if last node exist and having isEndOfWord true then only return found true
+        return (rootTrav != null && rootTrav.isEndOfWord);
+    }
+
+    public int[] solve(String[] A, String[] B) {
+        //putting new root value in global static root
+        int[] res = new int[B.length];
+        root = new TrieNode();
+        //add all to trie
+        for (String temp: A){
+            insert(temp);
+        }
+        // check if work exist in trie or not, 0/1 from search function
+        for (int i = 0; i < B.length; i++) {
+            if (search(B[i])){
+                res[i] = 1;
             }else{
-                res.add(0);
+                res[i] = 0;
             }
         }
         return res;
