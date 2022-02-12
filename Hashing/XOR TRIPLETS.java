b6ca6fb1fda942c8c9d4bf1063c391368482dcc1
codetancode <tanay20gupta18@gmail.com>
2022-02-12 21:54:07 +0530

    XOR TRIPLETS O(n, n)

--- Hashing/XOR TRIPLETS.java
+++ Hashing/XOR TRIPLETS.java
@@ -1,33 +1,44 @@
 public class Solution {
-    //O(n^4), 3 basing i, j, k loops, into it, 2 loops for combined n elements traversal
-    //simple brute force
+    //O(n, n),
+   //idea is to find xor value 0 of a sub array and keep maintaining index difference for j
+   //we find 0 at k index, (k1-i1),(k2-i2)..
+   //nk-i1-i2..
     public int solve(ArrayList<Integer> A) {
-        int maxCount = 0;
-        //sinlple 3 loops i to n-1
+        // TriNode root = new TriNode();
+        // xorZeroFreq = 1;
+        // xorZeroIndex = 0;
+
+        HashMap<Integer, ArrayList<Integer>> xorvalTofrqIndexSum = new HashMap<>();
+        int mod = 1000000007;
+        int maxSub = 0;
+        int contXor = 0;
+        //adding 0 and its freq as 1 and sum of index as 0
+        xorvalTofrqIndexSum.put(0, new ArrayList<Integer>(2));
+        xorvalTofrqIndexSum.get(0).add(1);
+        xorvalTofrqIndexSum.get(0).add(0);
         for(int i=0;i < A.size();i++){
-            // j=i+1 to n-1
-            for(int j=i+1;j < A.size();j++){
-                //k=j to n-1
-                for(int k=j;k < A.size();k++){
-                    int xorij = 0;
-                    int xorjk = 0;
-                    //as we need continous XOR
-                    //getting xor value from i to j-1
-                    for(int c=i;c < j;c++){
-                        xorij^=A.get(c);
-                    }
-                    //getting xor value from j to k
-                    for(int c=j;c <= k;c++){
-                        xorjk^=A.get(c);
-                    }
-                    //check if xor is same or not
-                    if(xorij == xorjk){
-                        maxCount++;
-                    }
-                }
+            contXor^=A.get(i);
+
+            if(xorvalTofrqIndexSum.containsKey(contXor)){
+                int xorValFreq = xorvalTofrqIndexSum.get(contXor).get(0);
+                int xorValIndexSum = xorvalTofrqIndexSum.get(contXor).get(1);
+                //adding Freq of xor value*current index - (xorValueIndexSum)
+                maxSub = (maxSub%mod + (xorValFreq*(i))%mod-(xorValIndexSum)%mod )%mod;
+
+                //also update map for this contXorval
+                xorvalTofrqIndexSum.get(contXor).set(0, xorValFreq + 1);
+                //updating index sum as prev index + currIndex + 1(for 0 based indexing)
+                xorvalTofrqIndexSum.get(contXor).set(1, xorValIndexSum + i + 1);
+
+            }else{
+                xorvalTofrqIndexSum.put(contXor, new ArrayList<Integer>(2));
+                //adding 0 and its freq as 1 and sum of index as currIndex +1 for 0 based indexing
+                xorvalTofrqIndexSum.get(contXor).add(1);
+                xorvalTofrqIndexSum.get(contXor).add(i+1);
             }
+
         }
-        return maxCount;
+        return maxSub%mod;
 
         }
 }
