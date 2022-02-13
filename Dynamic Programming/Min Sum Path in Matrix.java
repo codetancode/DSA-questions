e3933b383cf568b29f98d9210f0f2f4dca3a6009
codetancode <tanay20gupta18@gmail.com>
2022-02-14 01:11:47 +0530

    Min Sum Path in Matrix O(n*m, n) space optimized best Dp

--- Dynamic Programming/Min Sum Path in Matrix.java
+++ Dynamic Programming/Min Sum Path in Matrix.java
@@ -1,28 +1,32 @@
 public class Solution {
-    //O(n*m, n*m) Dp Solution, space could be optimized by taking 2 rows
+    //O(n*m, n) Dp Solution, space optimized by taking 2 rows
     //idea is trying bottom up approach, from last row going up, and top left will have min cost
     // as oonly right and down move possible, updating last col with down values, and last row with its right values
     //for i, j cell, updating min(right and down)+current value
     public int minPathSum(ArrayList<ArrayList<Integer>> A) {
-        // ArrayList<Integer> bottomRow; = new ArrayList<Integer>();
-        // ArrayList<Integer> currRow; = new ArrayList<Integer>();
+        ArrayList<Integer> currRow;
+        ArrayList<Integer> bottomRow;
         int nRow = A.size()-1;
         int mCol = A.get(0).size()-1;
-        for(int i=nRow;i >= 0;i--){
-            for(int j=mCol;j >= 0;j--){
-                //if target cell skip
-                if(i == nRow && j == mCol){
-                    continue;
-                }else if(j == mCol){
-                //if right most col, with its down value
-                    A.get(i).set(j, A.get(i).get(j) + A.get(i+1).get(j));
-                }else if(i == nRow){
-                //if bottom row, with its right values
-                    A.get(i).set(j, A.get(i).get(j) + A.get(i).get(j+1));
-                }else{
-                    // for remaining i,j          min(right and down)+current value
-                    A.get(i).set(j, Math.min(A.get(i).get(j+1), A.get(i+1).get(j)) + A.get(i).get(j));
-                }
+        //fill last row
+        for(int j=mCol-1;j >= 0;j--){
+            //if bottom row, with its right values
+            A.get(nRow).set(j, A.get(nRow).get(j) + A.get(nRow).get(j+1));
+            }
+        //fill last col
+        for(int j=nRow-1;j >= 0;j--){
+            //if right most col, with its down value
+            A.get(j).set(mCol, A.get(j).get(mCol) + A.get(j+1).get(mCol));
+        }
+        // from 2nd last row as current i, and i+ row as bottom row,
+        // fill for col -1 element as last col is already filled
+        for(int i=nRow-1;i >= 0;i--){
+            currRow = A.get(i);
+            bottomRow = A.get(i+1);
+            for(int j=mCol-1;j>=0;j--){
+                // for remaining i,j          min(right and down)+current value
+            currRow.set(j, Math.min(currRow.get(j+1), bottomRow.get(j)) + currRow.get(j));
+
             }
         }
         //first cell will have the min cost
