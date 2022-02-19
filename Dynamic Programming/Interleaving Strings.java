76a995d787024d0b80e659db4b078e6962f65db1
codetancode <tanay20gupta18@gmail.com>
2022-02-20 02:55:35 +0530

    Interleaving Strings O(n*m, n*m) len of strings OptimizedDP memoization

--- Dynamic Programming/Interleaving Strings.java
+++ Dynamic Programming/Interleaving Strings.java
@@ -1,36 +1,46 @@
 public class Solution {
-    //brute force recursive
-    //dp top down recursive solution
-    //idea is to have 3 pointers to 3 strings, and for same char in both A and B, try incrementing in both and check it it satisfies
+    //dp bottom up best
+    //idea is to store intermedeate result of A's index with B's index into a 2d dp array,
+    //and as soon as we get those value return them in recursive calls
+    static int[][] dp;
+    //dp using meoization
     static boolean recFunTopDown(int pointA, int pointB, int pointC, String A, String B, String C){
         // base condition, if all reaches end
         if(pointC == C.length() && pointA == A.length() && pointB == B.length()){
             return true;
         }
-        //if c reaches end and not A and B
+        //if only c reaches end not A and B
         if(pointC == C.length()){
             return false;
         }
-
-        //2 operation for either incrementing A, or B for same char in C
+        //if dp array has values for given pointA, pointB, return from here directly
+        if( dp[pointA][pointB] != -1 ){
+            return (dp[pointA][pointB]==1)?true:false;
+        }
+        //if not then make 2 operations either extend A or B for common char in C
         boolean op1 = false;
         boolean op2 = false;
-        //incrementing A
-        //check valid point A , then same char at C
         if(pointA < A.length() && C.charAt(pointC) == A.charAt(pointA) ){
             op1 = recFunTopDown(pointA + 1, pointB, pointC + 1, A, B, C);
         }
-        //incrementing B
-        //check valid point B , then same char at C
         if(pointB < B.length() && C.charAt(pointC) == B.charAt(pointB) ){
             op2 = recFunTopDown(pointA , pointB + 1, pointC + 1, A, B, C);
         }
-
-        //return OR of both operations
-        return op1|op2;
-
+        //fill result of given pointA and pointB in DP array
+        dp[pointA][pointB] = (op1|op2)?1:0;
+        //return if possible by either of the operations
+        return (op1|op2);
     }
+
     public int isInterleave(String A, String B, String C) {
+        //depends on constraints, take extra 5 size
+        dp = new int[105][105];
+        // init with dp array for all with -1
+        for(int i=0;i < 105;i++){
+            for(int j=0;j < 105;j++){
+                dp[i][j] = -1;
+            }
+        }
         return recFunTopDown(0, 0, 0, A, B, C)?1:0;
     }
 }
