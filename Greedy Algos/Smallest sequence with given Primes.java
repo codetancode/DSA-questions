081bfcc787e648221713fdd640cced17bbe7bdbe
codetancode <tanay20gupta18@gmail.com>
2022-04-11 10:15:15 +0530

    Smallest sequence with given Primes O(d, 1) storing 3 array for prime, natural multiplers from 1(currIndex), next best answer, only. Arrayd of size d is part of output so 1 space complexity

--- Greedy Algos/Smallest sequence with given Primes.java
+++ Greedy Algos/Smallest sequence with given Primes.java
@@ -1,26 +1,32 @@
 public class Solution {
-//O(d*log(3*d), d) d times addding into heap , size of heap 3 times d
-//storing multiple into min heap taking out smallest adding multiple of 3 prime number into it
-   public ArrayList < Integer > solve(int p1, int p2, int p3, int k) {
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        ArrayList < Integer > res = new ArrayList < Integer >();
-        minHeap.add(p1);
-        minHeap.add(p2);
-        minHeap.add(p3);
-        res.add(minHeap.peek());
+    public ArrayList < Integer > solve(int p1, int p2, int p3, int k) {
+        ArrayList < Integer > finalAnswer = new ArrayList < Integer > ();
+        int[] nextBestNumbers = new int[3];
+        // natural number multiplicatio for all 3 prime are stores in current index(1, 2, 3, ..) for 2 prime no.
+        int[] currIndex = new int[3];
+        int[] prime = new int[3];
 
-        while(res.size() < k){
-            int smallestNum = minHeap.poll();
+        nextBestNumbers[0] = prime[0] = p1;
+        nextBestNumbers[1] = prime[1] = p2;
+        nextBestNumbers[2] = prime[2] = p3;
+
+        currIndex[0] = currIndex[1] = currIndex[2] = -1;
+
+        for (int j = 1; j <= k; j++) {
+            int nextNumber = Math.min(nextBestNumbers[0], Math.min(nextBestNumbers[1], nextBestNumbers[2]));
+            finalAnswer.add(nextNumber);
+
             //to avoid duplicates
-            if(res.size() > 0 && res.get(res.size()-1) != smallestNum){
-                res.add(smallestNum);
+            for (int i = 0; i < 3; i++) {
+                if (nextNumber == nextBestNumbers[i]) {
+                    //increaseing multipliers for that prime number
+                    currIndex[i]++;
+                    // adding increased index multiplication of that ith prime nuo. into the choices(nextBest array)
+                    nextBestNumbers[i] = finalAnswer.get(currIndex[i]) * prime[i];
+                }
             }
-            //adding all multiples
-            minHeap.add(p1*smallestNum);
-            minHeap.add(p2*smallestNum);
-            minHeap.add(p3*smallestNum);
         }
+        return finalAnswer;
 
-        return res;
     }
 }
